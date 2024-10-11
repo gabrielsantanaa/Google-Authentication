79c09be7bc7e7df36b1eb209408bebeff2278b25
Tiago Almeida <tiagoalmeidasaj@gmail.com>
2024-10-10 21:44:59 -0300

    Fix: ViewModel Constructor

--- app/src/main/java/com/gabrielsantana/letsvote/features/poll/ui/NewPollViewModel.kt
+++ app/src/main/java/com/gabrielsantana/letsvote/features/poll/ui/NewPollViewModel.kt
@@ -4,9 +4,10 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
+import javax.inject.Inject
 
 @HiltViewModel
-class NewPollViewModel : ViewModel() {
+class NewPollViewModel @Inject constructor(): ViewModel() {
 
     private val _uiState = MutableStateFlow(NewPollUiState.INITIAL)
     val uiState = _uiState
